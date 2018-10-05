f2a82a30e30ec76268880126becd2183640538b8
alumno Secundaria <al3201@cedi.local>
2018-10-05 09:10:36 -0500

    Se terminó la branch de Dibujo y se uede acceder a los horarios y otra información

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { HistoriaPage } from '../pages/historia/historia';
+import { CienciasPage } from '../pages/ciencias/ciencias';
+import { DibujoPage } from '../pages/dibujo/dibujo';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    HistoriaPage,
+    CienciasPage,
+    DibujoPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    HistoriaPage,
+    CienciasPage,
+    DibujoPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HistoriaPage } from '../historia/historia';
+import { CienciasPage } from '../ciencias/ciencias';
+import { DibujoPage } from '../dibujo/dibujo';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  historia = HistoriaPage;
+  ciencias = CienciasPage;
+  dibujo = DibujoPage;
+  constructor(public navCtrl: NavController) {
+
+  }
+clickHistoria(){
+  this.navCtrl.push(this.historia);
+}
+clickCiencias(){
+  this.navCtrl.push(this.ciencias);
+}
+clickDibujo(){
+  this.navCtrl.push(this.dibujo);
+}
+}
